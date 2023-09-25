267c859b163675dbbda981eec13c88dc2aad1645
Felipe Carillo <felipecarillo@outlook.com>
2023-09-25 11:10:46 -0300

    Developing create_user.py

--- iac/iac/mss_auction/iac_stack.py
+++ iac/iac/mss_auction/iac_stack.py
@@ -7,6 +7,7 @@ from aws_cdk import (
 from constructs import Construct
 from .lambda_stack import LambdaStack
 
+
 class IACStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
@@ -28,13 +29,13 @@ class IACStack(Stack):
             },
         )
 
-        restapi_resourse= self.__restapi.root.add_resource("apae-leilao", default_cors_preflight_options=
-            {
-                "allow_origins": apigw.Cors.ALL_ORIGINS,
-                "allow_methods": ["GET", "POST", "PUT", "DELETE"],
-                "allow_headers": ["*"],
-            }
-        )
+        restapi_resourse = self.__restapi.root.add_resource("apae-leilao", default_cors_preflight_options=
+        {
+            "allow_origins": apigw.Cors.ALL_ORIGINS,
+            "allow_methods": ["GET", "POST", "PUT", "DELETE"],
+            "allow_headers": ["*"],
+        }
+                                                            )
 
         ENVIROMENT_VARIABLES = {
             "STAGE": stage,
@@ -43,6 +44,4 @@ class IACStack(Stack):
         }
 
         self.__lambda_function = LambdaStack(self, restapi_resource=restapi_resourse,
-                                           environment_variables=ENVIROMENT_VARIABLES)
-
-
+                                             environment_variables=ENVIROMENT_VARIABLES)
