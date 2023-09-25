e48d7e5b21c59efc93ecc0a42807c6b9ed85ba09
Felipe Carillo <felipecarillo@outlook.com>
2023-09-25 12:46:43 -0300

    Developing create_user.py

--- iac/iac/mss/lambda_stack.py
+++ iac/iac/mss/lambda_stack.py
@@ -14,7 +14,6 @@ class LambdaStack(Stack):
                       environment_variables: Dict[str, str]) -> _lambda.Function:
         function = _lambda.Function(
             self, function_name.title(),
-            function_name=function_name.title(),
             environment=environment_variables,
             runtime=_lambda.Runtime.PYTHON_3_9,
             code=_lambda.Code.from_asset(f"../src/modules/{function_name}"),
@@ -30,7 +29,7 @@ class LambdaStack(Stack):
 
     def __init__(self, scope: Construct, restapi_resource: apigw.Resource,
                  environment_variables: Dict[str, str]) -> None:
-        super().__init__(scope, "ApaeLeilao-Lambdas")
+        super().__init__(scope, "ApaeLeilao_Lambdas")
 
         self.create_user = self.create_lambda(
             function_name="create_user",
