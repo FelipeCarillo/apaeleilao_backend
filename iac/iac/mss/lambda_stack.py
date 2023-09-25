8e8e76926aeb38f4de02e611a0303fd5f65fd502
Felipe Carillo <felipecarillo@outlook.com>
2023-09-25 15:47:37 -0300

    Developing create_user.py

--- iac/iac/mss/lambda_stack.py
+++ iac/iac/mss/lambda_stack.py
@@ -13,8 +13,8 @@ class LambdaStack(Stack):
     def create_lambda(self, function_name: str, method: str, restapi_resource: apigw.Resource,
                       environment_variables: Dict[str, str]) -> _lambda.Function:
         function = _lambda.Function(
-            self, function_name.title(),
-            function_name=function_name.title(),
+            self, function_name.title() + "_apae_leilao",
+            function_name=function_name.title() + "_apae_leilao",
             environment=environment_variables,
             runtime=_lambda.Runtime.PYTHON_3_9,
             code=_lambda.Code.from_asset(f"../src/modules/{function_name}"),
