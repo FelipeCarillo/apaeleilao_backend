367357a25d0b4ee48ca2803f278670a2c6f2f629
Felipe Carillo <felipecarillo@outlook.com>
2023-10-02 14:33:01 -0300

    Developing create_user.py

--- src/shared/database/database.py
+++ src/shared/database/database.py
@@ -1,12 +1,12 @@
 import os
-from pymongo.mongo_client import MongoClient
+import pymongo
 
 class Database:
     def __init__(self):
         user = os.getenv('MONGODB_USER')
         password = os.getenv('MONGODB_PASSWORD')
         credentials = f"mongodb+srv://{user}:{password}@apaeleilaoimt.vv5d9ja.mongodb.net/?retryWrites=true&w=majority"
-        self.__database_connection = MongoClient(credentials).get_database('apaeleilaoimt')
+        self.__database_connection = pymongo.MongoClient(credentials).get_database('apaeleilaoimt')
 
     def get_table_user(self):
         table = self.__database_connection['user']
