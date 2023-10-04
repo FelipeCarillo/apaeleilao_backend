26e443c88ea2b0e7aec6734088b90b79f7c200c0
Felipe Carillo <felipecarillo@outlook.com>
2023-10-04 18:58:13 -0300

    Developing API get_user.py

--- iac/iac/iac_stack.py
+++ iac/iac/iac_stack.py
@@ -17,7 +17,7 @@ class IACStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        stage = os.environ.get("STAGE", "TEST").upper()
+        stage = os.environ.get("STAGE", "test")
         bucket_name = os.environ.get("BUCKET_NAME", "")
         self.__bucket = s3.Bucket(
             self, f"Apae_Leilao_Bucket_{stage}",
