7b27bcbe8c97370131a1c82dda7ef29b8d14428f
Felipe Carillo <felipecarillo@outlook.com>
2023-10-04 13:06:15 -0300

    Developing API get_user.py

--- src/shared/structure/enums/user_enum.py
+++ src/shared/structure/enums/user_enum.py
@@ -17,13 +17,6 @@ class STATUS_USER_ACCOUNT_ENUM(Enum):
     PENDING = 'PENDING'
     BANED = 'BANED'
 
-    @classmethod
-    def get_value(cls, value):
-        for enum_item in cls:
-            if enum_item.value == value:
-                return enum_item
-        return None
-
 
 
 
