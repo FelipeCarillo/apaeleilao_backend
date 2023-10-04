bff0e4fab03a1c449f971234d486f3f2c7f79183
Felipe Carillo <felipecarillo@outlook.com>
2023-10-04 10:40:45 -0300

    Developing API get_user.py

--- iac/iac/lambda_stack.py
+++ iac/iac/lambda_stack.py
@@ -14,12 +14,6 @@ class LambdaStack(Construct):
     def create_lambda(self, function_name: str, method: str, restapi_resource: apigw.Resource,
                       environment_variables: Dict[str, str]) -> _lambda.Function:
 
-        shared_layer = _lambda.LayerVersion(
-            self, "ApaeLeilao_Layer",
-            code=_lambda.Code.from_asset("./apaeleilao_layer"),
-            compatible_runtimes=[_lambda.Runtime.PYTHON_3_9]
-        )
-
         function = _lambda.Function(
             self, (function_name + "_apae_leilao").title(),
             function_name=(function_name + "_apae_leilao").title(),
@@ -27,7 +21,7 @@ class LambdaStack(Construct):
             runtime=_lambda.Runtime.PYTHON_3_9,
             code=_lambda.Code.from_asset(f"../src/modules/{function_name}"),
             handler=f"app.{function_name}_presenter.lambda_handler",
-            layers=[shared_layer],
+            layers=[self.shared_layer],
             timeout=Duration.seconds(15),
         )
 
@@ -40,6 +34,12 @@ class LambdaStack(Construct):
     def __init__(self, scope: Construct, restapi_resource: apigw.Resource,
                  environment_variables: Dict[str, str]) -> None:
         super().__init__(scope, "ApaeLeilao_Lambdas")
+
+        self.shared_layer = _lambda.LayerVersion(
+            self, "ApaeLeilao_Layer",
+            code=_lambda.Code.from_asset("./apaeleilao_layer"),
+            compatible_runtimes=[_lambda.Runtime.PYTHON_3_9]
+        )
 
         self.create_user = self.create_lambda(
             function_name="create_user",
