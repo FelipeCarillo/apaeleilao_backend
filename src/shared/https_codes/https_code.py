1f687329af88ac2f407dd6628a364dc8be348fa7
Felipe Carillo <felipecarillo@outlook.com>
2023-10-04 17:38:16 -0300

    Developing API get_user.py

--- src/shared/https_codes/https_code.py
+++ src/shared/https_codes/https_code.py
@@ -1,15 +1,14 @@
 import json
-from typing import Any
+from typing import Any, Dict
 
 
 class HttpResponse:
-    def __init__(self, status_code: int, body: Any):
+    def __init__(self, status_code: int, body: Dict):
         self.status_code = status_code
         self.body = body
 
     def to_dict(self):
         return {'statusCode': self.status_code,
-                'headers': {'Content-Type': 'application/json'},
                 'body': self.body}
 
 
