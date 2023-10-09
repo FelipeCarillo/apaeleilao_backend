06e35a52ce171b2c8810e464cb2f10010eac3937
Felipe Carillo <felipecarillo@outlook.com>
2023-10-09 10:14:00 -0300

    Developing API get_user.py

--- src/shared/structure/entities/suspension.py
+++ src/shared/structure/entities/suspension.py
@@ -1,4 +1,4 @@
-from src.shared.errors.entities_errors import SuspensionEntityError
+from src.shared.errors.modules_errors import InvalidParameter
 from src.shared.structure.enums.suspension_enum import STATUS_SUSPENSION_ENUM
 
 
@@ -29,39 +29,39 @@ class Suspension:
     @staticmethod
     def validade_and_set_suspension_id(suspension_id: str) -> str:
         if not suspension_id:
-            raise SuspensionEntityError('suspension_id is required.')
+            raise InvalidParameter('suspension_id', 'is required.')
         if not isinstance(suspension_id, str):
-            raise SuspensionEntityError('suspension_id must be a str.')
+            raise InvalidParameter('suspension_id', 'must be a str.')
         return suspension_id
 
     @staticmethod
     def validate_and_set_date_suspencion(date_suspencion: int) -> int:
         if not date_suspencion:
-            raise SuspensionEntityError('date_suspension is required.')
+            raise InvalidParameter('date_suspencion', 'is required.')
         if not isinstance(date_suspencion, int):
-            raise SuspensionEntityError('date_suspension must be a int.')
+            raise InvalidParameter('date_suspencion', 'must be a int.')
         return date_suspencion
 
     @staticmethod
     def validate_and_set_date_reactivation(date_reactivation: int) -> int:
         if not date_reactivation:
-            raise SuspensionEntityError('date_reactivation is required.')
+            raise InvalidParameter('date_reactivation', 'is required.')
         if not isinstance(date_reactivation, int):
-            raise SuspensionEntityError('date_reactivation must be a int.')
+            raise InvalidParameter('date_reactivation', 'must be a int.')
         return date_reactivation
 
     @staticmethod
     def validate_and_set_reason(reason: str) -> str:
         if not reason:
-            raise SuspensionEntityError('reason is required.')
+            raise InvalidParameter('Justificativa', 'é obrigatória.')
         if not isinstance(reason, str):
-            raise SuspensionEntityError('reason must be a str.')
+            raise InvalidParameter('reason', 'must be a str.')
         return reason
 
     @staticmethod
     def validate_and_set_status(status: STATUS_SUSPENSION_ENUM) -> STATUS_SUSPENSION_ENUM:
         if not status:
-            raise SuspensionEntityError('status is required.')
+            raise InvalidParameter('status is required.')
         if not isinstance(status, STATUS_SUSPENSION_ENUM):
-            raise SuspensionEntityError('status must be a STATUS_SUSPENSION_ENUM.')
+            raise InvalidParameter('status must be a STATUS_SUSPENSION_ENUM.')
         return status
