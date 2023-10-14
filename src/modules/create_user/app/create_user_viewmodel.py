4496ebdd3f1c3788d84b600bd4cb2f999312d37c
Felipe Carillo <felipecarillo@outlook.com>
2023-10-14 12:18:23 -0300

    Fixing create_user_usecase.py

--- src/modules/create_user/app/create_user_viewmodel.py
+++ src/modules/create_user/app/create_user_viewmodel.py
@@ -5,7 +5,7 @@ class CreateUserViewModel:
     def __call__(self, body: Dict) -> Dict:
         user = body['body']
         return {
-            'email': user['email'],
+            'user_id': user['user_id'],
             'password': user['password'],
             'type_account': user['type_account'],
         }
