9342bacf6cca11b9ea0c28fd8e2037c93b18b862
Felipe Carillo <felipecarillo@outlook.com>
2023-10-16 11:31:48 -0300

    Some changes in the backend and infra

--- src/shared/helper_functions/token_authy.py
+++ src/shared/helper_functions/token_authy.py
@@ -8,21 +8,21 @@ from src.shared.helper_functions.time_manipulation import TimeManipulation
 
 class TokenAuthy(ABC):
     def __init__(self):
-        self.__secret = os.getenv('ENCRYPTED_KEY')
-        self.__algorithm = os.getenv('JWT_ALGORITHM')
+        self.__secret = os.environ.get('ENCRYPTED_KEY')
+        self.__algorithm = os.environ.get('JWT_ALGORITHM')
 
     def encode(self, user_id: str) -> str:
-        time = TimeManipulation()
-        return jwt.encode({"user_id": user_id, "exp": time.plus_day(30)}, self.__secret,
+        date_exp = TimeManipulation().plus_day(30)
+        return jwt.encode({"user_id": user_id, "exp": date_exp}, self.__secret,
                           algorithm=self.__algorithm)
 
     def decode(self, token: str) -> Dict:
         return jwt.decode(token, self.__secret, algorithms=[self.__algorithm])
 
     def check_exp(self, token: str) -> bool:
-        time = TimeManipulation()
+        time_now = TimeManipulation().get_current_time()
         payload = self.decode(token)
-        return payload['exp'] > time.get_current_time()
+        return payload['exp'] > time_now
 
     def check_user_id(self, token: str, user_id: str) -> bool:
         payload = self.decode(token)
