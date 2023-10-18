ddd34e5168f0005f84c7d899c502675130412aab
MelipeFoon <felipe.t.moon@gmail.com>
2023-10-18 17:41:17 -0300

    fixing get_auctions_menu

---
+++ src/modules/get_auctions_menu/app/get_auctions_menu_controller.py
@@ -0,0 +1,31 @@
+from typing import Dict
+
+from get_auctions_menu_usecase import GetAuctionsMenuUseCase
+
+from src.shared.https_codes.https_code import *
+from src.shared.errors.modules_errors import *
+
+class GetAuctionsMenuController:
+    def __init__(self, usecase: GetAuctionsMenuUseCase):
+        self.__usecase = usecase
+
+    def __call__(self):
+        try:
+            get_auction_menu_usecase = self.__usecase()
+
+            return OK(body=get_auction_menu_usecase, message="Leilão encontrado com sucesso.")
+        
+        except DataNotFound as e:
+            return NotFound(message=e.message)
+        
+        except InvalidRequest as e:
+            return BadRequest(message=e.message)
+
+        except InvalidParameter as e:
+            return ParameterError(message=e.message)
+
+        except MissingParameter as e:
+            return BadRequest(message=e.message)
+
+        except Exception as e:
+            return InternalServerError(message=e.args[0])
---
+++ src/modules/get_auctions_menu/app/get_auctions_menu_presenter.py
@@ -0,0 +1,16 @@
+from .get_auctions_menu_controller import GetAuctionsMenuController
+from .get_auctions_menu_usecase import GetAuctionsMenuUseCase
+
+from src.shared.https_codes.https_code import HttpResponse, HttpRequest
+from src.shared.database.database_auction_table import AuctionDynamodb
+
+usecase = GetAuctionsMenuUseCase(AuctionDynamodb())
+controller = GetAuctionsMenuController(usecase)
+
+
+def lambda_handler(event, context):
+    request = HttpRequest()
+    response = controller(request=request())
+    http_response = HttpResponse(status_code=response.status_code, body=response.body)
+
+    return http_response.to_dict()
