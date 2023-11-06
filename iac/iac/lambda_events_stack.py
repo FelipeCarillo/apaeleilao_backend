3891e05b6ab764d4b565cb158219d5cf97d84704
Felipe Carillo <felipecarillo@outlook.com>
2023-11-06 19:39:38 -0300

    Removing useless layers from triggers lambdas

--- iac/iac/lambda_events_stack.py
+++ iac/iac/lambda_events_stack.py
@@ -18,7 +18,7 @@ class LambdaEventsStack(Construct):
             runtime=_lambda.Runtime.PYTHON_3_9,
             code=_lambda.Code.from_asset(f"../src/modules/{function_name}"),
             handler=f"app.{function_name}_presenter.lambda_handler",
-            layers=[self.shared_layer, self.jwt_layer, self.bcrypt_layer, self.mercadopago],
+            layers=[self.shared_layer, self.mercadopago],
             timeout=Duration.seconds(15),
             memory_size=512,
         )
@@ -28,18 +28,6 @@ class LambdaEventsStack(Construct):
     def __init__(self, scope: Construct,
                  environment_variables: Dict[str, str]) -> None:
         super().__init__(scope, "ApaeLeilao_Lambdas_Events")
-
-        self.bcrypt_layer = _lambda.LayerVersion(
-            self, "Bcrypt_Layer",
-            code=_lambda.Code.from_asset("./bcrypt_layer"),
-            compatible_runtimes=[_lambda.Runtime.PYTHON_3_9],
-        )
-
-        self.jwt_layer = _lambda.LayerVersion(
-            self, "Jwt_Layer",
-            code=_lambda.Code.from_asset("./jwt_layer"),
-            compatible_runtimes=[_lambda.Runtime.PYTHON_3_9]
-        )
 
         self.shared_layer = _lambda.LayerVersion(
             self, "ApaeLeilao_Layer",
