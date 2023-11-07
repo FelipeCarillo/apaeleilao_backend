557c7eae6757a65502263709e52cfd0bef428ec6
Felipe Carillo <felipecarillo@outlook.com>
2023-11-06 23:17:03 -0300

    adding print in event trigger

--- src/shared/helper_functions/events_trigger.py
+++ src/shared/helper_functions/events_trigger.py
@@ -59,7 +59,7 @@ class EventsTrigger:
                 ]
             )
         except Exception as e:
-            return e
+            print(e)
 
     def delete_rule(self, rule_name: str, lambda_function: str):
 
@@ -82,7 +82,7 @@ class EventsTrigger:
                 Name=rule_name,
             )
         except Exception as e:
-            return e
+            print(e)
 
     @staticmethod
     def __format_date(date: int) -> str:
