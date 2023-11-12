3142aa8b43902052225ccfb25ac5d20d13ef9aa3
Felipe Carillo <felipecarillo@outlook.com>
2023-11-12 16:23:30 -0300

    Fixing end_auction

--- src/modules/end_auction/app/end_auction_presenter.py
+++ src/modules/end_auction/app/end_auction_presenter.py
@@ -1,10 +1,11 @@
 from .end_auction_usecase import EndAuctionUseCase
 from .end_auction_controller import EndAuctionController
 
+from src.shared.database.database_user import UserDynamodb
 from src.shared.database.database_auction import AuctionDynamodb
 from src.shared.https_codes.https_code import HttpRequest, HttpResponse
 
-usecase = EndAuctionUseCase(AuctionDynamodb())
+usecase = EndAuctionUseCase(AuctionDynamodb(), UserDynamodb())
 controller = EndAuctionController(usecase)
 
 
