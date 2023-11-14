9ffb9371416cec4183b70cab59ed18cbfcac951b
Felipe Carillo <felipecarillo@outlook.com>
2023-11-14 19:25:46 -0300

    Fixing Image Manipulation

--- src/shared/helper_functions/image_manipulation.py
+++ src/shared/helper_functions/image_manipulation.py
@@ -5,22 +5,22 @@ import base64
 
 class ImageManipulation:
     def __init__(self):
-        self.__s3 = boto3.resource('s3')
+        self.__s3 = boto3.client('s3')
         self.__auction_folder = 'auctions/'
         self.__bucket = os.environ.get('BUCKET_NAME')
 
     def create_auction_folder(self, auction_id: str):
         try:
             auction_folder_key = f'{self.__auction_folder}{auction_id}/'
-            self.__s3.Object(self.__bucket, auction_folder_key).put(Body='')
+            self.__s3.put_object(ACL='public-read', Bucket=self.__bucket, Key=auction_folder_key, Body='')
         except Exception as e:
             raise e
 
     def upload_auction_image(self, auction_id: str, image_id: str, image_body: str, content_type: str):
         try:
             image_key = f'{self.__auction_folder}{auction_id}/{image_id}'
-            self.__s3.put_object(Bucket=self.__bucket, Key=image_key,
-                                 Body=base64.b64decode(image_body), ContentType=content_type)
+            self.__s3.put_object(ACL='public-read', Bucket=self.__bucket, Key=image_key, Body=base64.b64decode(image_body),
+                                 ContentType=content_type)
             return self.get_image_url(image_id)
         except Exception as e:
             raise e
